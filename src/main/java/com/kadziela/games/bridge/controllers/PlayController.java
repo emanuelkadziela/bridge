e9985ea1602d1d052dbba500b6a104a565418992
Emanuel Kadziela <emanuel.kadziela@gmail.com>
2020-04-04 01:44:17 -0400

    added a little bit of code towads playing the game after bidding
    added a little bit of code towads playing the game after bidding

--- src/main/java/com/kadziela/games/bridge/controllers/PlayController.java
+++ src/main/java/com/kadziela/games/bridge/controllers/PlayController.java
@@ -1,34 +1,45 @@
 package com.kadziela.games.bridge.controllers;
 
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+
+import com.kadziela.games.bridge.model.Card;
 import com.kadziela.games.bridge.service.RoomService;
+import com.kadziela.games.bridge.util.MapUtil;
 
 @Controller
 public class PlayController 
 {
 	private static final Logger logger = LogManager.getLogger(PlayController.class);
 
-	@Autowired private RoomService roomService;
 	@Autowired private SimpMessageSendingOperations messagingTemplate;
 	
-	@MessageMapping("/TBD")	
-	public void TBD(String name)
+	/**
+	 * Plays a card from a player
+	 * @param attributes the map of attributes needed to execute the call, must include the card (suit and rank) and the position which played it.
+	 */
+	@MessageMapping("/play/card")	
+	public void playCard(Map<String,String> attributes)
 	{
-		logger.debug(String.format("Room Controller received the request for %s to enter the room", name));
+		logger.debug(String.format("Play Controller received the request to play a card with these attributes", attributes));
 	    try
-	    {
+	    {	
+			Assert.notNull(attributes, "the map of attributes cannot be null");
 			
-		    messagingTemplate.convertAndSend("/queue/private/"+name, "Hello new player ");
 	    }
 	    catch (IllegalArgumentException iae)
 	    {
-	    	logger.error("Unable to create a new player with username: "+ name+", because that name is already in use ", iae);
-		    messagingTemplate.convertAndSend("/topic/errors","Unable to create a new player with username: "+ name+", because that name is already in use ");	    	
+	    	logger.error("IllegalArgumentExceptiom occurred while trying to play a card ", iae);
+	    	Map<String,String> errorMap = MapUtil.mappifyStringMessage("IllegalArgumentExceptiom occurred while trying to play a card ");
+	    	errorMap.put("error",iae.getMessage());
+	    	messagingTemplate.convertAndSend("/topic/errors",errorMap);	    	
 	    }	    
 	}
 }
