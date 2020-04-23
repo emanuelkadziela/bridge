3fe8638bc49a5348dda5ad1c6c032da4071f3e82
Emanuel Kadziela <emanuel.kadziela@gmail.com>
2020-04-23 15:25:52 -0400

    more test refatoring
    put everything common to most tests in one util class to stop repeating myself

--- src/test/java/com/kadziela/games/bridge/EnterRoomCreateTableSitDownNiceGPTest.java
+++ src/test/java/com/kadziela/games/bridge/EnterRoomCreateTableSitDownNiceGPTest.java
@@ -1,54 +1,20 @@
 package com.kadziela.games.bridge;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.lang.reflect.Type;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.Transport;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-
-import com.google.gson.Gson;
 import com.kadziela.games.bridge.handlers.ErrorStompFrameHandler;
 import com.kadziela.games.bridge.handlers.QueueStompFrameHandler;
-import com.kadziela.games.bridge.model.Message;
-import com.kadziela.games.bridge.model.Player;
-import com.kadziela.games.bridge.model.Table;
 import com.kadziela.games.bridge.service.TableService;
 import com.kadziela.games.bridge.util.TestUtils;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EnterRoomCreateTableSitDownNiceGPTest 
 {
-	private static final Logger logger = LogManager.getLogger(EnterRoomCreateTableSitDownNiceGPTest.class);
-	
 	 @Value("${local.server.port}")
 	 private int port;
 	 private String URL;
@@ -60,55 +26,6 @@ public class EnterRoomCreateTableSitDownNiceGPTest
 	 @Test
 	 public void testCreateGameEndpoint() throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException 
 	 {
-		StompSession stompSession = TestUtils.setupTest(URL, port,errorStompFrameHandler,queueStompFrameHandler);
-		Long tableId = enterRoomAndCreateTable(stompSession);	    
-		sitDown(tableId,stompSession);		
-	 }
-	 private void sitDown(Long tableId, StompSession stompSession) throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException
-	 {
-			stompSession.subscribe("/topic/table/"+tableId, queueStompFrameHandler);
-			sitPlayer("North","NORTH",String.valueOf(tableId),stompSession);
-			sitPlayer("South","SOUTH",String.valueOf(tableId),stompSession);
-			sitPlayer("East","EAST",String.valueOf(tableId),stompSession);
-			List<Map<String,Object>> messages = sitPlayer("West","WEST",String.valueOf(tableId),stompSession);						
-			Map<String,Object> mm = messages.get(1);
-			assertNotNull(mm.get("cardsBySeat"));			
-			mm = messages.get(2);
-			assertNotNull(mm.get("dealer"));						
-	 }
-	 private List<Map<String,Object>> sitPlayer(String name, String position, String table, StompSession stompSession) throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException
-	 {
-			Map<String,String> attributes = new HashMap<String,String>();
-			attributes.put("playerName", name);
-			attributes.put("tableId", table);
-			attributes.put("position", position);
-			stompSession.send("/app/table/sitDown", attributes);
-			List<Map<String,Object>> messages = queueStompFrameHandler.getMessages();
-			logger.info("Player {} attempted to sit in the {} seat at table {}, the /topic/table/{} channel received the following messages: {} ",name,position,table,table,messages);
-			assertNotNull(messages);
-			Map<String,Object> mm = messages.get(0);
-			assertNotNull(mm);
-			assertEquals(name, mm.get("playerName"));
-			assertEquals(position, mm.get("position"));
-			assertEquals(table, mm.get("tableId"));
-			assertTrue(((String) mm.get("message")).contains("successfully sat down"));
-			return messages;
-	 }
-	 private Long enterRoomAndCreateTable(StompSession stompSession) throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException
-	 {			
-		 	TestUtils.roomEnterPlayer("North", stompSession,queueStompFrameHandler);
-		 	TestUtils.roomEnterPlayer("South", stompSession,queueStompFrameHandler);
-		 	TestUtils.roomEnterPlayer("East", stompSession,queueStompFrameHandler);
-		 	TestUtils.roomEnterPlayer("West", stompSession,queueStompFrameHandler);
-		 	Long externalId = System.currentTimeMillis();
-			stompSession.send("/app/table/openNewWithExternalId", externalId);
-			List<Map<String,Object>> messages = queueStompFrameHandler.getMessages();
-			assertNotNull(messages);
-			Map<String,Object> map = messages.get(0);						
-			logger.info("Attempted to open a new table with external id {}, this message was sent to the /topic/room channel: {} ",externalId,map);
-			assertNotNull(map);
-			Table table = tableService.findByExternalId(externalId);
-			logger.info("table = {} ", table);
-			return table.getId();
+		 TestUtils.put4PlayersInRoomAndTable(URL, port, errorStompFrameHandler, queueStompFrameHandler, tableService, "North","South","East","West");
 	 }
 }
--- src/test/java/com/kadziela/games/bridge/util/TestUtils.java
+++ src/test/java/com/kadziela/games/bridge/util/TestUtils.java
@@ -2,6 +2,7 @@ package com.kadziela.games.bridge.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -28,8 +29,10 @@ import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import com.kadziela.games.bridge.EnterRoomCreateTableSitDownNiceGPTest;
 import com.kadziela.games.bridge.handlers.ErrorStompFrameHandler;
 import com.kadziela.games.bridge.handlers.QueueStompFrameHandler;
+import com.kadziela.games.bridge.model.Table;
 import com.kadziela.games.bridge.model.enumeration.SeatPosition;
 import com.kadziela.games.bridge.model.enumeration.ValidBidOption;
+import com.kadziela.games.bridge.service.TableService;
 
 public class TestUtils 
 {
@@ -92,5 +95,66 @@ public class TestUtils
 			if (name2.contentEquals(name)) break;
 		}
 		assertEquals(name,name2);		 
+	 }
+	 public static final void roomEnterPlayers(StompSession stompSession, QueueStompFrameHandler queueStompFrameHandler, String ... names) throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException
+	 {
+		 Stream.of(names).forEach(name -> {
+			try {roomEnterPlayer(name,stompSession,queueStompFrameHandler);} 
+			catch (URISyntaxException | InterruptedException | ExecutionException | TimeoutException e) 
+			{
+				logger.error("exception occurred while entering players into the room ",e);
+			}
+		});
+	 }
+	 public static final Long createTable(StompSession stompSession, QueueStompFrameHandler queueStompFrameHandler, TableService tableService) throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException
+	 {			
+		 Long externalId = System.currentTimeMillis();
+		 stompSession.send("/app/table/openNewWithExternalId", externalId);
+		 List<Map<String,Object>> messages = queueStompFrameHandler.getMessages();
+		 assertNotNull(messages);
+		 Map<String,Object> map = messages.get(0);						
+		 logger.info("Attempted to open a new table with external id {}, this message was sent to the /topic/room channel: {} ",externalId,map);
+		 assertNotNull(map);
+		 Table table = tableService.findByExternalId(externalId);
+		 logger.info("table = {} ", table);
+		 stompSession.subscribe("/topic/table/"+table.getId(), queueStompFrameHandler);
+		 return table.getId();
+	 }
+	 public static final List<Map<String,Object>> sitPlayer(String name, String position, String table, StompSession stompSession, QueueStompFrameHandler queueStompFrameHandler) throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException
+	 {
+		Map<String,String> attributes = new HashMap<String,String>();
+		attributes.put("playerName", name);
+		attributes.put("tableId", table);
+		attributes.put("position", position);
+		stompSession.send("/app/table/sitDown", attributes);
+		List<Map<String,Object>> messages = queueStompFrameHandler.getMessages();
+		logger.info("Player {} attempted to sit in the {} seat at table {}, the /topic/table/{} channel received the following messages: {} ",name,position,table,table,messages);
+		assertNotNull(messages);
+		Map<String,Object> mm = messages.get(0);
+		assertNotNull(mm);
+		assertEquals(name, mm.get("playerName"));
+		assertEquals(position, mm.get("position"));
+		assertEquals(table, mm.get("tableId"));
+		assertTrue(((String) mm.get("message")).contains("successfully sat down"));
+		return messages;
+	 }
+	 public static final void sit4PlayersDown(Long tableId, StompSession stompSession, QueueStompFrameHandler queueStompFrameHandler, String ... names) throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException
+	 {
+			TestUtils.sitPlayer(names[0],"NORTH",String.valueOf(tableId),stompSession,queueStompFrameHandler);
+			TestUtils.sitPlayer(names[1],"SOUTH",String.valueOf(tableId),stompSession,queueStompFrameHandler);
+			TestUtils.sitPlayer(names[2],"EAST",String.valueOf(tableId),stompSession,queueStompFrameHandler);
+			List<Map<String,Object>> messages = TestUtils.sitPlayer(names[3],"WEST",String.valueOf(tableId),stompSession,queueStompFrameHandler);						
+			Map<String,Object> mm = messages.get(1);
+			assertNotNull(mm.get("cardsBySeat"));			
+			mm = messages.get(2);
+			assertNotNull(mm.get("dealer"));						
+	 }
+	 public static final Long put4PlayersInRoomAndTable(String URL, int port, ErrorStompFrameHandler errorStompFrameHandler,QueueStompFrameHandler queueStompFrameHandler, TableService tableService, String ... names) throws URISyntaxException, InterruptedException, ExecutionException, TimeoutException
+	 {
+		 StompSession stompSession = TestUtils.setupTest(URL, port,errorStompFrameHandler,queueStompFrameHandler);	
+		 TestUtils.roomEnterPlayers(stompSession, queueStompFrameHandler, names);
+		 Long tableId = TestUtils.createTable(stompSession,queueStompFrameHandler,tableService);	    
+		 TestUtils.sit4PlayersDown(tableId, stompSession, queueStompFrameHandler, names);	
+		 return tableId;
 	 }
 }
